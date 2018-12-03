4ac6ba5d5bbde4f2aa74e0701a4bcf455ce63df4
Krutharth <ckrutharth23@gmail.com>
2018-12-03 19:36:55 +0530

    beginPlay override successful (lesson 128) (previous mistakes are rectified)

---
+++ battleTank/Source/battleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("player controller is not possesing the tank"))
+	}
+
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("player controller is possesing: %s"), *(ControlledTank->GetName()))
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const {
+	return Cast<ATank>(GetPawn());
+}
+
+
