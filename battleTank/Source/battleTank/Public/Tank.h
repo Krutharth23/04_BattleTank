3e63093cae8d88c29acaf4284ad49f48f6ea626c
Krutharth <ckrutharth23@gmail.com>
2019-04-20 07:16:31 +0530

    health bar done 217 done

--- battleTank/Source/battleTank/Public/Tank.h
+++ battleTank/Source/battleTank/Public/Tank.h
@@ -18,6 +18,9 @@ public:
 
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEvent, class AController * EventInstigator, AActor * DamageCauser) override;
 
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetHealthPercent() const;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
